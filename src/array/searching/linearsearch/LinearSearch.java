f250cb90f68ca4e62e17ff02903663a989ac00d9
Rajat Pandey <rajatpandey7800@gmail.com>
2023-12-30 00:29:54 +0530

    Linear Search

--- src/array/searching/linearsearch/LinearSearch.java
+++ src/array/searching/linearsearch/LinearSearch.java
@@ -11,6 +11,7 @@ public class LinearSearch {
         }
         return -1;
     }
+
     public static void main(String[] args) {
         int arr[] = {1, 3, 4, 5, 6, 8, -2, 34};
         int n = arr.length;
