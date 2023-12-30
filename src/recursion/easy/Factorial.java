b99fd086a9bec125e081b785ee21b39deb02e5c2
Rajat Pandey <rajatpandey7800@gmail.com>
2023-12-30 23:28:03 +0530

    Recursion

---
+++ src/recursion/easy/Factorial.java
@@ -0,0 +1,18 @@
+package recursion.easy;
+
+import java.util.ArrayList;
+
+public class Factorial {
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }
+
+        int smallOutput = factorial(n - 1);
+        return n * smallOutput;
+    }
+    public static void main(String[] args){
+        int fact = factorial(5);
+        System.out.println(fact);
+    }
+}
