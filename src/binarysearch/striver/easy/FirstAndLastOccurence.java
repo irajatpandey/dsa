16cdab1c39bfd8e400bdab5c8b62451241985333
Rajat Pandey <rajatpandey7800@gmail.com>
2024-02-07 23:30:59 +0530

    BinarySearch

---
+++ src/binarysearch/striver/easy/FirstAndLastOccurence.java
@@ -0,0 +1,56 @@
+package binarysearch.striver.easy;
+
+public class FirstAndLastOccurence {
+
+    public static int firstOccurrence(int[] nums, int target){
+       int start = 0, end = nums.length - 1, outputIndex = -1;
+       while (start <= end){
+           int mid = start + (end - start)/2;
+           if(nums[mid] == target){
+               outputIndex = mid;
+               end = mid - 1;
+           }
+           else if(nums[mid] > target){
+               end = mid - 1;
+           }
+           else{
+               start = mid + 1;
+           }
+       }
+       return outputIndex == -1 ? outputIndex : nums[outputIndex];
+    }
+
+    public static int lastOccurrence(int[] nums, int target){
+        int start = 0, end = nums.length - 1, outputIndex = -1;
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                outputIndex = mid;
+                start = mid + 1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+
+        return outputIndex == -1 ? outputIndex : nums[outputIndex];
+    }
+    public static int[] firstAndLastOccurence(int[] nums, int target){
+        int[] ans = new int[2];
+        ans[0] = firstOccurrence(nums, target);
+        ans[1] = lastOccurrence(nums, target);
+
+        return ans;
+    }
+    public static void main(String[] args) {
+//      int[] nums = {5,7,7,8,8,10};
+        int target = 6;
+        int[] a = {0, 0};
+
+        int []ans = firstAndLastOccurence(a, target);
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}
--- src/binarysearch/striver/easy/UpperBound.java
+++ src/binarysearch/striver/easy/UpperBound.java
@@ -4,20 +4,20 @@ public class UpperBound {
     public static int upperBound(int[] arr, int n, int target) {
 
         int start = 0, end = n - 1;
-        int lower_bound = -1;
+        int upper_bound = -1;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
             if (arr[mid] > target) {
-                lower_bound = mid;
+                upper_bound = mid;
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
 
         }
-        return lower_bound;
+        return upper_bound;
 
     }
 
