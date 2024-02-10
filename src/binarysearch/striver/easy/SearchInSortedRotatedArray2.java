8b360a73cc687a8ba6b3d89b3b45e2329a865c93
Rajat Pandey <rajatpandey7800@gmail.com>
2024-02-10 10:35:05 +0530

    BinarySearch

--- src/binarysearch/striver/easy/SearchInSortedRotatedArray2.java
+++ src/binarysearch/striver/easy/SearchInSortedRotatedArray2.java
@@ -1,16 +1,19 @@
 package binarysearch.striver.easy;
 
-public class SearchInSortedRotatedArray {
-    // T.C O(logn)
-    // S.C O(1)
-    //https://leetcode.com/problems/search-in-rotated-sorted-array/description/
-    public static int searchInSortedRotatedArray(int[] nums, int target){
+// T.C O(logn) for average case and O(n) for the worst case
+// S.C O(1)
+//https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+public class SearchInSortedRotatedArray2 {
+    public static boolean searchInSortedRotatedArray2(int[] nums, int target){
         int start = 0, end = nums.length - 1;
 
         while(start <= end){
+            while(start < end && nums[start] == nums[start + 1]) start++;
+            while(start < end && nums[end] == nums[end - 1]) end--;
+
             int mid = start + (end - start)/2;
             if(nums[mid] == target){
-                return mid;
+                return true;
             }
             // is Left Sorted
             else if(nums[start] <= nums[mid] ){
@@ -28,14 +31,14 @@ public class SearchInSortedRotatedArray {
                 }
             }
         }
-        return -1;
+        return false;
     }
     public static void main(String[] args) {
-        int[] nums = {4,5,6,7,0,1,2};
+        int[] nums = {2,5,6,0,0,1,2};
         int n = nums.length;
 
         int target = 0;
-        int ans = searchInSortedRotatedArray(nums, target);
+        boolean ans = searchInSortedRotatedArray2(nums, target);
         System.out.println(ans);
     }
 }
