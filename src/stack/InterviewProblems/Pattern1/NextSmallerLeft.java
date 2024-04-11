7ba01d28c5b973d465d1e487215ba592ef0050ce
Rajat Pandey <rajatpandey7800@gmail.com>
2024-04-11 19:33:44 +0530

    Stack

---
+++ src/stack/InterviewProblems/Pattern1/NextSmallerLeft.java
@@ -0,0 +1,39 @@
+package stack.InterviewProblems.Pattern1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
+
+public class NextSmallerLeft {
+    public static ArrayList<Integer> nextSmallerElement(ArrayList<Integer> arr, int n){
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i < arr.size(); i++){
+            if(stack.isEmpty()){
+                res.add(-1);
+            }
+            else if(!stack.isEmpty() && stack.peek() < arr.get(i)){
+                res.add(stack.peek());
+            }
+            else if(!stack.isEmpty() && stack.peek() >= arr.get(i)){
+                while(!stack.isEmpty() && stack.peek() >= arr.get(i)){
+                    stack.pop();
+                }
+                if(stack.isEmpty()){
+                    res.add(-1);
+                }else{
+                    res.add(stack.peek());
+                }
+            }
+            stack.push(arr.get(i));
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.addAll(Arrays.asList(3, 1, 2, 4));
+        System.out.println(nextSmallerElement(arr, 3));
+    }
+}
--- src/stack/InterviewProblems/Pattern1/SumOfSubarrayMinimums.java
+++ src/stack/InterviewProblems/Pattern1/SumOfSubarrayMinimums.java
@@ -1,58 +1,44 @@
 package stack.InterviewProblems.Pattern1;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
 //https://leetcode.com/problems/sum-of-subarray-minimums/
 //Time complexity: O(n)
 // Space Complexity : O(n) // stack
 public class SumOfSubarrayMinimums {
+    public static ArrayList<Integer> nextSmallerRight(ArrayList<Integer> arr, int n){
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = n - 1; i >= 0; i--){
+            if(stack.isEmpty()){
+                res.add(n);
+            }
+            else if(!stack.isEmpty() && stack.peek() < arr.get(i)){
+                res.add(stack.peek());
+            }
+            else if(!stack.isEmpty() && stack.peek() >= arr.get(i)){
+                while(!stack.isEmpty() && stack.peek() >= arr.get(i)){
+                    stack.pop();
+                }
+                if(stack.isEmpty()){
+                    res.add(n);
+                }else{
+                    res.add(stack.peek());
+                }
+            }
+            stack.push(arr.get(i));
+        }
+        Collections.reverse(res);
+        return res;
+    }
     public static int sumSubarrayMins(int[] arr) {
         int length = arr.length;
-        // Arrays to keep track of previous smaller and next smaller elements
-        int[] left = new int[length];
-        int[] right = new int[length];
+        ArrayList<Integer> ls =  Arrays.stream(arr).boxed().collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<Integer> left = NextSmallerLeft.nextSmallerElement(ls, ls.size());
+        ArrayList<Integer> right = nextSmallerRight(ls, ls.size());
 
-        // Initialize left array with -1 indicating the start of array
-        Arrays.fill(left, -1);
-        // Initialize right array with length of array indicating the end of array
-        Arrays.fill(right, length);
 
-        // Stack to keep track of elements while traversing
-        Deque<Integer> stack = new ArrayDeque<>();
-
-        // Calculate previous smaller elements for each element in the array
-        for (int i = 0; i < length; ++i) {
-            // Popping all elements which are greater than the current element
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-                stack.pop();
-            }
-            // The current top of the stack indicates the previous smaller element
-            if (!stack.isEmpty()) {
-                left[i] = stack.peek();
-            }
-            // Push the current index into the stack
-            stack.push(i);
-        }
-
-        for(int ele : left) {
-            System.out.print(ele + " ");
-        }
-        System.out.println();
-        // Clear the stack for next traversal
-        stack.clear();
-
-        // Calculate next smaller elements for each element in the array in reverse order
-        for (int i = length - 1; i >= 0; --i) {
-            // Popping all elements which are greater than or equal to the current element
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-                stack.pop();
-            }
-            // The current top of the stack indicates the next smaller element
-            if (!stack.isEmpty()) {
-                right[i] = stack.peek();
-            }
-            // Push the current index into the stack
-            stack.push(i);
-        }
 
         // The mod value for big integer operations to prevent overflow
         int mod = (int) 1e9 + 7;
@@ -63,7 +49,7 @@ public class SumOfSubarrayMinimums {
         for (int i = 0; i < length; ++i) {
             // Total count of subarrays where arr[i] is min is (i - left[i]) * (right[i] - i)
             // Multiply the count by the value itself and apply modulo operation
-            answer += (long) (i - left[i]) * (right[i] - i) % mod * arr[i] % mod;
+            answer += (long) (i - left.get(i)) * (right.get(i) - i) % mod * arr[i] % mod;
             // Ensure the running sum doesn't overflow
             answer %= mod;
         }
@@ -79,6 +65,8 @@ public class SumOfSubarrayMinimums {
     }
     public static void main(String[] args) {
         int[] arr = {3, 1, 2, 4};
-        sumSubarrayMins(arr);
+        int output = sumSubarrayMins(arr);
+        System.out.println(output);
+
     }
 }
