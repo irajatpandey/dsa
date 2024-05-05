2dc508665a1f9ccdb15cf3024dc829abb84a25b0
Rajat Pandey <rajatpandey7800@gmail.com>
2024-05-05 22:30:00 +0530

    Binary Tree

--- src/binaryTree/medium/BoundaryTraversal.java
+++ src/binaryTree/medium/BoundaryTraversal.java
@@ -1,10 +1,88 @@
 package binaryTree.medium;
 
+import binaryTree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+//Time Complexity: O(n)
+//Space Complexity: O(n + h), where n is the number of nodes and h is the height of the tree.
 public class BoundaryTraversal {
-    public static void boundaryTraversal() {
+    public static void traverseLeft(TreeNode<Integer> root, List<Integer> list) {
+        // Base Condition
+        if (root == null || root.left == null && root.right == null) {
+            return;
+        }
+
+        list.add(root.data);
+        if(root.left != null) {
+            traverseLeft(root.left, list);
+        }
+        else{
+            traverseLeft(root.right, list);
+        }
 
     }
-    public static void main(String[] args) {
+    public static void traverseLeaf(TreeNode<Integer> root, List<Integer> list) {
+        // Base Condition
+        if (root == null) {
+            return;
+        }
+        if(root.left == null && root.right == null) {
+            list.add(root.data);
+        }
 
+        traverseLeaf(root.left, list);
+        traverseLeaf(root.right, list);
+
+    }
+    public static void traverseRight(TreeNode<Integer> root, List<Integer> list) {
+        // Base Condition
+        if (root == null || root.left == null && root.right == null) {
+            return;
+        }
+
+        if(root.right != null) {
+            traverseRight(root.right, list);
+        }
+        else {
+            traverseRight(root.left, list);
+        }
+        list.add(root.data);
+    }
+    public static void boundaryTraversal(TreeNode<Integer> root) {
+        List<Integer> list = new ArrayList<>();
+        list.add(root.data);
+        traverseLeft(root.left, list);
+        traverseLeaf(root.left, list);
+        traverseLeaf(root.right, list);
+        traverseRight(root.right, list);
+        System.out.println(list);
+    }
+    public static void main(String[] args) {
+        /*        10
+                /    \
+               7      20
+              / \    /  \
+             4   9  15   25
+                / \      / \
+               8  11    22  30
+                        / \
+                       21  24
+        */
+        TreeNode<Integer> root = new TreeNode<>(10);
+        root.left = new TreeNode<>(7);
+        root.right = new TreeNode<>(20);
+        root.left.left = new TreeNode<>(4);
+        root.left.right = new TreeNode<>(9);
+        root.left.right.left = new TreeNode<>(8);
+        root.left.right.right = new TreeNode<>(11);
+        root.right.left = new TreeNode<>(15);
+        root.right.right = new TreeNode<>(25);
+        root.right.right.left = new TreeNode<>(22);
+        root.right.right.left.left = new TreeNode<>(21);
+        root.right.right.left.right = new TreeNode<>(24);
+        root.right.right.right = new TreeNode<>(30);
+        System.out.println("Boundary Traversal:");
+        boundaryTraversal(root);
     }
 }
--- src/binaryTree/medium/VerticalOrderTraversal.java
+++ src/binaryTree/medium/VerticalOrderTraversal.java
@@ -2,9 +2,69 @@ package binaryTree.medium;
 
 import binaryTree.TreeNode;
 
+import java.util.*;
+//https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/description/
+//The time complexity is dominated by the traversal of all nodes (O(N)) and the sorting of nodes within each group (O(N log N) in the worst case).
+//The space complexity depends on the number of horizontal distances and levels, as well as the space required for the result. It can be O(N^2) in the worst case but is typically lower in balanced trees.
+class PairOfNodeAndHorizontalDistance{
+    TreeNode<Integer> node;
+    int level;
+    int horizontalDistance;
+
+    PairOfNodeAndHorizontalDistance(TreeNode<Integer> node, int level, int horizontalDistance){
+        this.node = node;
+        this.horizontalDistance = horizontalDistance;
+        this.level = level;
+    }
+}
 public class VerticalOrderTraversal {
-    public static void verticalOrderTraversal(TreeNode<Integer> root) {
-        
+    public static void printVerticalOrder(Map<Integer, Map<Integer, List<Integer>>> map, Queue<PairOfNodeAndHorizontalDistance> queue){
+        while(!queue.isEmpty()){
+            PairOfNodeAndHorizontalDistance temp = queue.poll();
+            TreeNode<Integer> node = temp.node;
+            int horizontalDistance = temp.horizontalDistance;
+            int level = temp.level;
+
+            if (!map.containsKey(horizontalDistance)) {
+                map.put(horizontalDistance, new TreeMap<>());
+            }
+            Map<Integer, List<Integer>> levelMap = map.get(horizontalDistance);
+            if (!levelMap.containsKey(level)) {
+                levelMap.put(level, new ArrayList<>());
+            }
+            List<Integer> list = levelMap.get(level);
+            list.add(node.data);
+            levelMap.put(level, list);
+
+            if(temp.node.left != null){
+                queue.add(new PairOfNodeAndHorizontalDistance(node.left, level + 1, horizontalDistance - 1));
+            }
+            if(temp.node.right != null){
+                queue.add(new PairOfNodeAndHorizontalDistance(node.right, level + 1, horizontalDistance + 1));
+            }
+        }
+    }
+    public static List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        Queue<PairOfNodeAndHorizontalDistance> queue = new LinkedList<>();
+        Map<Integer, Map<Integer, List<Integer>>> map = new TreeMap<>();
+
+        queue.offer(new PairOfNodeAndHorizontalDistance(root, 0, 0));
+
+        printVerticalOrder(map, queue);
+
+        for(int hd : map.keySet()){
+            Map<Integer, List<Integer>> levelMap = map.get(hd);
+            List<Integer> tempList = new ArrayList<>();
+            for (List<Integer> levelList : levelMap.values()) {
+                Collections.sort(levelList);
+                tempList.addAll(levelList);
+            }
+            result.add(tempList);
+        }
+
+        return result;
     }
     public static void main(String[] args) {
 	/* Example Binary Tree:
@@ -25,6 +85,9 @@ public class VerticalOrderTraversal {
         root.right.right = new TreeNode<>(7);
         root.left.right.left = new TreeNode<>(8);
         root.left.right.right = new TreeNode<>(9);
+
+        List<List<Integer>> output = verticalTraversal(root);
+        System.out.println(output);
 
     }
 }
